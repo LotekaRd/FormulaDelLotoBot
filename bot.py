748d60e50cd17feff268c323ef85be74c4aa28fa
LotekaRd <enzo.diot17@gmail.com>
2025-04-16 03:26:02 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,24 +1,15 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler
 
-# Remplacez ceci par votre token
+import asyncio
+from telegram.ext import Application
+
 TOKEN = " 7606091350:AAGEKVoR0E-D5rdRQk36LIwdHGlDhlXD4Hw "
 
-# Fonction pour démarrer le bot
-async def start(update: Update, context):
-    await update.message.reply("Bonjour, je suis votre bot!")
-
-# Fonction principale pour lancer l'application
 async def main():
-    # Créez l'application et passez le token
     application = Application.builder().token(TOKEN).build()
-
-    # Ajoutez un gestionnaire de commande "/start"
-    application.add_handler(CommandHandler("start", start))
-
-    # Démarrer le bot
+    # Ajouter vos gestionnaires ici
     await application.run_polling()
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+# Si une boucle d'événements est déjà en cours, utilisez asyncio.get_event_loop()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
